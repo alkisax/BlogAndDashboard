c4d3db765182eb03baf5150bfd9ee9dab55a99bc
Alkis <alkisax@gmail.com>
2025-07-19 19:02:19 +0300

    deploy8

--- frontend/src/pages/homepageComponents/IntroHome.jsx
+++ frontend/src/pages/homepageComponents/IntroHome.jsx
@@ -4,9 +4,9 @@ const IntroHome = () => {
   return (
     <>
     <div
-      className="flex flex-col items-center text-center px-4 w-3/5"
+      className="flex flex-col items-center text-center px-4 w-4/5"
     >
-      <img src="/welcome.png" alt="" />
+      <img src="/welcome.png" alt="" className="w-2/5"/>
       <p className="max-w-3xl mx-auto text-justify leading-relaxed text-gray-800 text-lg px-4 sm:px-6 md:px-8">
         Welcome to our blog — a vibrant space dedicated to sharing knowledge, stories, and fresh perspectives across a wide range of topics. Whether you're here to stay updated with the latest trends, discover new ideas, or simply enjoy well-crafted articles, you’ll find something to spark your interest. Our team is passionate about delivering content that is insightful, engaging, and easy to understand, with new posts added regularly. We invite you to explore our categories, dive into in-depth articles, and join a community of curious minds eager to learn and share. Thank you for visiting, and we hope you enjoy your journey through our content!
       </p>       
